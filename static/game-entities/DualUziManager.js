82d56f380b4b9cc028743ded9f9c2427fbbbca82
Andrew Chan <andrewkchan.akc@gmail.com>
2018-08-08 23:04:57 -0700

    Make dual wield use 1 weapon gameobject

--- static/game-entities/DualUziManager.js
+++ static/game-entities/DualUziManager.js
@@ -9,20 +9,16 @@ function DualUziManager(player) {
   this.player._weapon.fireRate = 120; // ms between each shot in autofire mode
   this.player._weapon.bulletAngleVariance = 10; // degrees of shot spread
 
-  player._weapon2 = game.add.weapon(0, "particle");
-  this.player._weapon2.bulletClass = RevolverBullet;
-  this.player._weapon2.createBullets(-1);
-  this.player._weapon2.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-  this.player._weapon2.bulletSpeed = 250;
-  this.player._weapon2.fireRate = 120; // ms between each shot in autofire mode
-  this.player._weapon2.bulletAngleVariance = 10; // degrees of shot spread
+  this.dualWieldToggle = true;
+  this.player._weapon.onFire.add(function(weapon, bullet) {
+    this.dualWieldToggle = !this.dualWieldToggle;
+  }, this);
 }
 
 DualUziManager.prototype = Object.create(WeaponManager);
 DualUziManager.prototype.constructor = DualUziManager;
 DualUziManager.prototype.destroy = function() {
   this.player._weapon.destroy();
-  this.player._weapon2.destroy();
   this.player._weaponHandU.destroy();
   this.player._weaponHandD.destroy();
   this.player._weaponHandL.destroy();
@@ -38,9 +34,9 @@ DualUziManager.prototype.initForegroundAnims = function() {
   player._weaponHandR = player.addChild(game.make.sprite(7, 9, 'empty_convict_hand'));
   player._weaponHandR.anchor.setTo(0.5, 0.5);
   player._weaponHandR._recoilTween =
-    game.add.tween(player._weaponHandR).to({ x: 4, rotation: '-0.1' }, 20, Phaser.Easing.Linear.None);
+    game.add.tween(player._weaponHandR).to({ x: 4 }, 20, Phaser.Easing.Linear.None);
   player._weaponHandR._recoilTween.chain(
-    game.add.tween(player._weaponHandR).to({ x: 7, rotation: '+0.1' }, 50, Phaser.Easing.Linear.None)
+    game.add.tween(player._weaponHandR).to({ x: 7 }, 50, Phaser.Easing.Linear.None)
   );
   player._weaponHandR._gun = player._weaponHandR.addChild(game.make.sprite(0, 0, 'uzi'));
   player._weaponHandR._gun.anchor.set(0.35, 0.45);
@@ -50,9 +46,9 @@ DualUziManager.prototype.initForegroundAnims = function() {
   player._weaponHandR2 = player.addChild(game.make.sprite(-2, 9, 'empty_convict_hand'));
   player._weaponHandR2.anchor.setTo(0.5, 0.5);
   player._weaponHandR2._recoilTween =
-    game.add.tween(player._weaponHandR2).to({ x: -5, rotation: '-0.1' }, 20, Phaser.Easing.Linear.None);
+    game.add.tween(player._weaponHandR2).to({ x: -5 }, 20, Phaser.Easing.Linear.None);
   player._weaponHandR2._recoilTween.chain(
-    game.add.tween(player._weaponHandR2).to({ x: -2, rotation: '+0.1' }, 50, Phaser.Easing.Linear.None)
+    game.add.tween(player._weaponHandR2).to({ x: -2 }, 50, Phaser.Easing.Linear.None)
   );
   player._weaponHandR2._gun = player._weaponHandR2.addChild(game.make.sprite(0, 0, 'uzi'));
   player._weaponHandR2._gun.anchor.set(0.35, 0.45);
@@ -62,9 +58,9 @@ DualUziManager.prototype.initForegroundAnims = function() {
   player._weaponHandL = player.addChild(game.make.sprite(-6, 9, 'empty_convict_hand'));
   player._weaponHandL.anchor.setTo(0.5, 0.5);
   player._weaponHandL._recoilTween =
-    game.add.tween(player._weaponHandL).to({ x: -3, rotation: '+0.1' }, 20, Phaser.Easing.Linear.None);
+    game.add.tween(player._weaponHandL).to({ x: -3 }, 20, Phaser.Easing.Linear.None);
   player._weaponHandL._recoilTween.chain(
-    game.add.tween(player._weaponHandL).to({ x: -6, rotation: '-0.1' }, 50, Phaser.Easing.Linear.None)
+    game.add.tween(player._weaponHandL).to({ x: -6 }, 50, Phaser.Easing.Linear.None)
   );
   player._weaponHandL._gun = player._weaponHandL.addChild(game.make.sprite(0, 0, 'uzi_flipped'));
   player._weaponHandL._gun.anchor.set(0.35, 0.55);
@@ -75,9 +71,9 @@ DualUziManager.prototype.initForegroundAnims = function() {
   player._weaponHandL2 = player.addChild(game.make.sprite(2, 9, 'empty_convict_hand'));
   player._weaponHandL2.anchor.setTo(0.5, 0.5);
   player._weaponHandL2._recoilTween =
-    game.add.tween(player._weaponHandL2).to({ x: 5, rotation: '+0.1' }, 20, Phaser.Easing.Linear.None);
+    game.add.tween(player._weaponHandL2).to({ x: 5 }, 20, Phaser.Easing.Linear.None);
   player._weaponHandL2._recoilTween.chain(
-    game.add.tween(player._weaponHandL2).to({ x: 2, rotation: '-0.1' }, 50, Phaser.Easing.Linear.None)
+    game.add.tween(player._weaponHandL2).to({ x: 2 }, 50, Phaser.Easing.Linear.None)
   );
   player._weaponHandL2._gun = player._weaponHandL2.addChild(game.make.sprite(0, 0, 'uzi_flipped'));
   player._weaponHandL2._gun.anchor.set(0.35, 0.55);
@@ -88,9 +84,9 @@ DualUziManager.prototype.initForegroundAnims = function() {
   player._weaponHandD = player.addChild(game.make.sprite(6, 9, 'empty_convict_hand'));
   player._weaponHandD.anchor.setTo(0.5, 0.5);
   player._weaponHandD._recoilTween =
-    game.add.tween(player._weaponHandD).to({ y: 5, rotation: '-0.1' }, 20, Phaser.Easing.Linear.None);
+    game.add.tween(player._weaponHandD).to({ y: 5 }, 20, Phaser.Easing.Linear.None);
   player._weaponHandD._recoilTween.chain(
-    game.add.tween(player._weaponHandD).to({ y: 9, rotation: '+0.1' }, 50, Phaser.Easing.Linear.None)
+    game.add.tween(player._weaponHandD).to({ y: 9 }, 50, Phaser.Easing.Linear.None)
   );
   player._weaponHandD._gun = player._weaponHandD.addChild(game.make.sprite(0, 0, 'uzi'));
   player._weaponHandD._gun.anchor.set(0.35, 0.45);
@@ -100,9 +96,9 @@ DualUziManager.prototype.initForegroundAnims = function() {
   player._weaponHandD2 = player.addChild(game.make.sprite(-2, 9, 'empty_convict_hand'));
   player._weaponHandD2.anchor.setTo(0.5, 0.5);
   player._weaponHandD2._recoilTween =
-    game.add.tween(player._weaponHandD2).to({ y: 5, rotation: '-0.1' }, 20, Phaser.Easing.Linear.None);
+    game.add.tween(player._weaponHandD2).to({ y: 5 }, 20, Phaser.Easing.Linear.None);
   player._weaponHandD._recoilTween.chain(
-    game.add.tween(player._weaponHandD2).to({ y: 9, rotation: '+0.1' }, 50, Phaser.Easing.Linear.None)
+    game.add.tween(player._weaponHandD2).to({ y: 9 }, 50, Phaser.Easing.Linear.None)
   );
   player._weaponHandD2._gun = player._weaponHandD2.addChild(game.make.sprite(0, 0, 'uzi'));
   player._weaponHandD2._gun.anchor.set(0.35, 0.45);
@@ -117,9 +113,9 @@ DualUziManager.prototype.initBackgroundAnims = function() {
   player._weaponHandU = player.addChild(game.make.sprite(-6, 8, 'empty_convict_hand'));
   player._weaponHandU.anchor.setTo(0.5, 0.5);
   player._weaponHandU._recoilTween =
-    game.add.tween(player._weaponHandU).to({ y: 11, rotation: '-0.5' }, 20, Phaser.Easing.Linear.None);
+    game.add.tween(player._weaponHandU).to({ y: 11 }, 20, Phaser.Easing.Linear.None);
   player._weaponHandU._recoilTween.chain(
-    game.add.tween(player._weaponHandU).to({ y: 8, rotation: '+0.5' }, 50, Phaser.Easing.Linear.None)
+    game.add.tween(player._weaponHandU).to({ y: 8 }, 50, Phaser.Easing.Linear.None)
   );
   player._weaponHandU._gun = player._weaponHandU.addChild(game.make.sprite(0, 0, 'uzi'));
   player._weaponHandU._gun.anchor.set(0.35, 0.45);
@@ -129,9 +125,9 @@ DualUziManager.prototype.initBackgroundAnims = function() {
   player._weaponHandU2 = player.addChild(game.make.sprite(1, 8, 'empty_convict_hand'));
   player._weaponHandU2.anchor.setTo(0.5, 0.5);
   player._weaponHandU2._recoilTween =
-    game.add.tween(player._weaponHandU).to({ y: 11, rotation: '-0.5' }, 20, Phaser.Easing.Linear.None);
+    game.add.tween(player._weaponHandU).to({ y: 11 }, 20, Phaser.Easing.Linear.None);
   player._weaponHandU2._recoilTween.chain(
-    game.add.tween(player._weaponHandU).to({ y: 8, rotation: '+0.5' }, 50, Phaser.Easing.Linear.None)
+    game.add.tween(player._weaponHandU).to({ y: 8 }, 50, Phaser.Easing.Linear.None)
   );
   player._weaponHandU2._gun = player._weaponHandU2.addChild(game.make.sprite(0, 0, 'uzi'));
   player._weaponHandU2._gun.anchor.set(0.35, 0.45);
@@ -169,41 +165,54 @@ DualUziManager.prototype.update = function(angle, isVisible = true) {
       player._weaponHandD2.rotation = angle;
       player._weaponHandD.visible = true;
       player._weaponHandD2.visible = true;
-      player._weapon.trackSprite(player._weaponHandD, 7, -3, true);
-      player._weapon2.trackSprite(player._weaponHandD2, 7, -3, true);
+      if (this.dualWieldToggle) {
+        player._weapon.trackSprite(player._weaponHandD, 10, -3, true);
+      } else {
+        player._weapon.trackSprite(player._weaponHandD2, 10, -3, true);
+      }
     } else if (MathUtil.isRight(angle)) {
       player._weaponHandR.rotation = angle;
       player._weaponHandR.visible = true;
       player._weaponHandR2.rotation = angle;
       player._weaponHandR2.visible = true;
-      player._weapon.trackSprite(player._weaponHandR, 7, -3, true);
-      player._weapon2.trackSprite(player._weaponHandR2, 7, -3, true);
+      if (this.dualWieldToggle) {
+        player._weapon.trackSprite(player._weaponHandR, 10, -3, true);
+      } else {
+        player._weapon.trackSprite(player._weaponHandR2, 10, -3, true);
+      }
     } else if (MathUtil.isUp(angle)) {
       player._weaponHandU.rotation = angle;
       player._weaponHandU.visible = true;
       player._weaponHandU2.rotation = angle;
       player._weaponHandU2.visible = true;
-      player._weapon.trackSprite(player._weaponHandU, 7, -3, true);
-      player._weapon2.trackSprite(player._weaponHandU2, 7, -3, true);
+      if (this.dualWieldToggle) {
+        player._weapon.trackSprite(player._weaponHandU, 15, -3, true);
+      } else {
+        player._weapon.trackSprite(player._weaponHandU2, 15, -3, true);
+      }
     } else {
       player._weaponHandL.rotation = angle;
       player._weaponHandL.visible = true;
       player._weaponHandL2.rotation = angle;
       player._weaponHandL2.visible = true;
-      player._weapon.trackSprite(player._weaponHandL, 7, 3, true);
-      player._weapon2.trackSprite(player._weaponHandL2, 7, 3, true);
+      if (this.dualWieldToggle) {
+        player._weapon.trackSprite(player._weaponHandL, 10, 3, true);
+      } else {
+        player._weapon.trackSprite(player._weaponHandL2, 10, 3, true);
+      }
     }
-
-    if (this.player._weapon.autofire) {
-      this.player._weapon.fireAtPointer(game.input.activePointer);
-      this.player._weapon2.fireAtPointer(game.input.activePointer);
-      this.recoilWeaponHand(GameInputUtil.getCursorAngle(game.input));
-    }
+    this.fireIfDown();
+  }
+};
+DualUziManager.prototype.fireIfDown = function() {
+  // tiny hack to fire with 1 weapon object from 2 guns
+  if (this.player._weapon.autofire && game.input.activePointer.isDown) {
+    this.player._weapon.fireAtPointer(game.input.activePointer);
+    this.recoilWeaponHand(GameInputUtil.getCursorAngle(game.input));
   }
 };
 DualUziManager.prototype.fire = function(input) {
   this.player._weapon.autofire = true;
-  WeaponManager.prototype.fire.call(this, input);
 };
 DualUziManager.prototype.fireStop = function(input) {
   this.player._weapon.autofire = false;
